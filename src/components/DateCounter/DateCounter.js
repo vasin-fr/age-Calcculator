86ef6c3375133469c83d7089a8475742d8846e3a
Oleksii Vasin <vasin@oleksiis-macbook-air.home>
2024-01-05 19:06:14 +0100

    setup for video with all styles

--- src/components/DateCounter/DateCounter.js
+++ src/components/DateCounter/DateCounter.js
@@ -1,5 +1,49 @@
+import React from "react";
+import "./_DateCounter.sass";
+
 const DateCounter = () => {
-  return <h1>Hi</h1>;
+  // пиздилка інпутів
+  // повертає дату
+
+  return (
+    <div className="block">
+      <div className="block__inputs">
+        <div className="error">
+          <label htmlFor="day">Day</label>
+          <input type="number" placeholder="DD" id="day" />
+          <p>Catch a Error</p>
+        </div>
+        <div>
+          <label htmlFor="month">Month</label>
+          <input type="number" placeholder="MM" id="month" />
+          <p>Catch a Error</p>
+        </div>
+        <div>
+          <label htmlFor="year">Year</label>
+          <input type="number" placeholder="YYYY" id="year" />
+          <p>Catch a Error</p>
+        </div>
+      </div>
+      <div className="block__divisor">
+        <div className="line"></div>
+        <button type="button"></button>
+      </div>
+      <ul className="block__results">
+        <li className="denomination">
+          <span>- -</span>
+          <div>years</div>
+        </li>
+        <li className="denomination">
+          <span>- -</span>
+          <div>months</div>
+        </li>
+        <li className="denomination">
+          <span>- -</span>
+          <div>days</div>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default DateCounter;
